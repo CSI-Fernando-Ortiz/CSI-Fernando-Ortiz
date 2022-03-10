be9c93b4d87898805b7bdc804a84638d7d042aa8
CSI-Fernando-Ortiz <93546130+CSI-Fernando-Ortiz@users.noreply.github.com>
2022-03-10 14:39:07 -0400

    Create Fullname.ino

---
+++ Fullname.ino
@@ -0,0 +1,197 @@
+int a=7;
+int b=6;
+int c=4;
+int d=2;
+int e=11;
+int f=9;
+int g=10;
+int dp=5;
+int h= 12;
+int decimal = 9;
+
+
+void setup() {
+ pinMode (a, OUTPUT);
+ pinMode (b, OUTPUT);
+ pinMode (c, OUTPUT);
+ pinMode (d, OUTPUT);
+ pinMode (e, OUTPUT);
+ pinMode (f, OUTPUT);
+ pinMode (g, OUTPUT);
+ pinMode (dp, OUTPUT); 
+ pinMode (decimal, OUTPUT);
+}
+
+void loop()  
+{
+ 
+  digitalWrite (a, HIGH);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, LOW);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, HIGH);
+  digitalWrite (dp, HIGH);
+
+ digitalWrite (decimal, HIGH);
+
+delay(1000);
+
+  
+  digitalWrite (a, HIGH);
+  digitalWrite (b, LOW);
+  digitalWrite (c, LOW);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, LOW);
+  digitalWrite (g, HIGH);
+  digitalWrite (dp, LOW);
+  
+  digitalWrite (decimal, HIGH);
+
+
+  delay(1000);
+
+  digitalWrite (a, HIGH);
+  digitalWrite (b, HIGH);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, LOW);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, HIGH);
+ digitalWrite (dp, HIGH);
+
+ digitalWrite (decimal, HIGH);
+
+  
+  delay(1000);
+
+ digitalWrite (a, HIGH);
+  digitalWrite (b, HIGH);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, LOW);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, LOW);
+ digitalWrite (dp, HIGH);
+
+ digitalWrite (decimal, HIGH);
+ 
+  delay(1000);
+
+  digitalWrite (a ,HIGH);
+  digitalWrite (b, HIGH);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, LOW);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, HIGH);
+ digitalWrite (dp, HIGH);
+
+ digitalWrite (decimal, HIGH);
+
+  delay(1000);
+
+  digitalWrite (a, HIGH);
+  digitalWrite (b, HIGH);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, LOW);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, LOW);
+ digitalWrite (dp, HIGH);
+
+ digitalWrite (decimal, HIGH);
+
+  delay(1000);
+
+  digitalWrite (a, HIGH);
+  digitalWrite (b, HIGH);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, LOW);
+ digitalWrite (dp, HIGH);
+
+delay(1000);
+
+   digitalWrite (a, HIGH);
+  digitalWrite (b, HIGH);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, LOW);
+ digitalWrite (dp, HIGH);
+
+
+   delay(1000);
+
+    digitalWrite (a, HIGH);
+  digitalWrite (b, HIGH);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, LOW);
+ digitalWrite (dp, HIGH);
+
+
+   delay(1000);
+
+
+digitalWrite (a, HIGH);
+  digitalWrite (b, HIGH);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, LOW);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, HIGH);
+  digitalWrite (g, HIGH);
+ digitalWrite (dp, HIGH);
+
+ digitalWrite (decimal, HIGH);
+
+  
+  delay(1000);
+
+digitalWrite (a, HIGH);
+  digitalWrite (b, HIGH);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, LOW);
+  digitalWrite (e, LOW);
+  digitalWrite (f, LOW);
+  digitalWrite (g, LOW);
+ digitalWrite (dp, HIGH);
+ digitalWrite (h, LOW);
+
+ digitalWrite (decimal, HIGH);
+
+  
+  delay(1000);
+  
+digitalWrite (a, HIGH);
+  digitalWrite (b, LOW);
+  digitalWrite (c, HIGH);
+  digitalWrite (d, LOW);
+  digitalWrite (e, LOW);
+  digitalWrite (f, LOW);
+  digitalWrite (g, LOW);
+ digitalWrite (dp, HIGH);
+
+
+   delay(1000);
+
+    digitalWrite (a, HIGH);
+  digitalWrite (b, HIGH);
+  digitalWrite (c, LOW);
+  digitalWrite (d, HIGH);
+  digitalWrite (e, HIGH);
+  digitalWrite (f, LOW);
+  digitalWrite (g, HIGH);
+ digitalWrite (dp, HIGH);
+
+
+   delay(1000);
+}
